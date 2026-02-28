24b2a20d071b4e4386167451be2bf48417f31538
Yingkai Niu <nicholas.t.e838@gmail.com>
2026-02-28 18:40:16 +0800

    changed the logic of graph finding

--- main.py
+++ main.py
@@ -1,466 +1,102 @@
 from manim import *
-
+from mobjects.path_finding_graph import PathFindingGraph
 
 
 class HamiltonPath(Scene):
-    def construct(self) :
-        #setting dots
-        dot_pos_list = [
-            [3.8378238140915517, -0.5733057553287386],#0
-            [3.9094422441839445, -1.50972436618488],#1
-            [1.22728219167647, 2.8840997591867765],#2
-            [- 0.9014937142731325, -2.886678029017851],#3
-            [- 4.4408807914835835, 0.38928870636404467],#4
-
-
-
-
-
-            [- 1.5190050841020142, -0.555995671348672],#5
-            [1.87625422654442, 1.3674150662044973],#6
-            [3.355581091063108, 2.8278229998944537],#7
-            [2.144193665966549, -2.8786121097563795],#8
-            [-1.6778929303556458, 0.9683144634387277],#9
-            [-2.467284852119872, -0.2263938676336097],#10
-            [-4.09644323847631, -1.0267798936875705],#11
-            [0.13104734778506, 1.2031149280588904],#12
-            [2.477545942167559, 0.582829322973079],#13
-            [0.7878419899872489, 0.7838878005782175],#14
-            [-4.611300071071119, 0.6147144346102609],#15
+    def construct(self):
+        # --- Phase 1: Small graph (5 nodes, 6 edges) ---
+        small_positions = [
+            [3.8378238140915517, -0.5733057553287386],   # 0
+            [3.9094422441839445, -1.50972436618488],     # 1
+            [1.22728219167647, 2.8840997591867765],      # 2
+            [-0.9014937142731325, -2.886678029017851],   # 3
+            [-4.4408807914835835, 0.38928870636404467],  # 4
         ]
-        dot_label = VGroup(
-            [Text(str(i)).move_to(RIGHT*dot_pos_list[i][0]+UP*dot_pos_list[i][1]) for i in range(len(dot_pos_list))]
-        )
         line_list = [
-            [0,1],
-            [1,2],
-            [2,3],
-            [3,0],
-            [2,4],
-            [3,4]
-
-
-
-
+            [0, 1],
+            [1, 2],
+            [2, 3],
+            [3, 0],
+            [2, 4],
+            [3, 4]
         ]
 
-        def get_index(u,v):
-            try:
-                return line_list.index([u, v])
-            except ValueError:
-                return line_list.index([v, u])
-
-
-        dots = VGroup(
-            *[Dot(point=RIGHT*dot_pos_list[i][0]+UP*dot_pos_list[i][1]) for i in range(len(dot_pos_list))]
+        small_graph = PathFindingGraph(
+            dot_positions=small_positions,
+            edges=line_list,
+            show_labels=False
         )
-        lines = VGroup(
-            *[Line(dots[line_list[i][0]], dots[line_list[i][1]]) for i in range(len(line_list))]
+        self.add(small_graph)
 
-        )
-        self.add(dots[0:4],lines,dot_label)
-        first_path_list = [0,1,2,3,4]
-        second_path_list = [4,3,0,1,2]
-        third_path_list = [3,4,2,3,0,1,2]
-        for i in range(len(first_path_list) - 1):
-            u = first_path_list[i]
-            v = first_path_list[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            try:
-                line_index = line_list.index([u, v])
-            except ValueError:
-                line_index = line_list.index([v, u])
-
-            self.play(
-                lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-        self.play(
-            *[dots[m].animate.set_color(RED) for m in range(len(first_path_list))],
-            *[lines[
-
-                  line_list.index(
-                      [first_path_list[m],
-                       first_path_list[m + 1]
-                       ]
-                  )
-              ].animate.set_color(RED) for m in range(len(first_path_list) - 1)
-              ]
-        )
-        self.play(
-            dots[0:5].animate.set_color(WHITE),
-            lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-
-
-
-        for i in range(len(second_path_list) - 1):
-            u = second_path_list[i]
-            v = second_path_list[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            try:
-                line_index = line_list.index([u, v])
-            except ValueError:
-                line_index = line_list.index([v, u])
-
-            self.play(
-                lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-        self.play(
-            *[dots[m].animate.set_color(RED) for m in range(len(second_path_list))],
-            *[lines[
-                 get_index(
-                     second_path_list[m],
-                     second_path_list[m+1]
-
-                )
-                ].animate.set_color(RED) for m in range(len(second_path_list) - 1)
-            ]
-        )
-
-
-        self.play(
-            dots[0:5].animate.set_color(WHITE),
-            lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-        for i in range(len(third_path_list) - 1):
-            u = third_path_list[i]
-            v = third_path_list[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            try:
-                line_index = line_list.index([u, v])
-            except ValueError:
-                line_index = line_list.index([v, u])
-
-            self.play(
-                lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-            self.wait(1)
-        self.play(
-            dots[0:5].animate.set_color(GREEN),
-            lines.animate.set_color(GREEN),
-            run_time=0.5
-        )
-        self.play(
-            dots[0:5].animate.set_color(WHITE),
-            lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
+        small_graph.show_non_hamilton_graph(self)
+        small_graph.show_hamilton_graph(self)
 
         self.wait(2)
 
-
-        #hamilton path
-        first_hamilton_path = [3,2,1,0]
-        second_hamilton_path = [3,2,4]
-        third_hamilton_path = [3,4,2,1,0,3]
-
-
-
-        for i in range(len(first_hamilton_path) - 1):
-            u = first_hamilton_path[i]
-            v = first_hamilton_path[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            line_index = get_index(u,v)
-
-            self.play(
-                lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-            self.wait(1)
-        self.play(
-            *[dots[m].animate.set_color(RED) for m in first_hamilton_path],
-            *[lines[
-                  get_index(
-                      first_hamilton_path[m],
-                      first_hamilton_path[m + 1]
-
-                  )
-              ].animate.set_color(RED) for m in range(len(first_hamilton_path) - 1)
-              ]
-        )
-
-        self.play(
-            dots[0:5].animate.set_color(WHITE),
-            lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-
-        self.wait(1)
-
-
-        for i in range(len(second_hamilton_path) - 1):
-            u = second_hamilton_path[i]
-            v = second_hamilton_path[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            line_index = get_index(u,v)
-
-            self.play(
-                lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-            self.wait(1)
-        self.play(
-            *[dots[m].animate.set_color(RED) for m in second_hamilton_path],
-            *[lines[
-                  get_index(
-                      second_hamilton_path[m],
-                      second_hamilton_path[m + 1]
-
-                  )
-              ].animate.set_color(RED) for m in range(len(second_hamilton_path) - 1)
-              ]
-        )
-
-        self.play(
-            dots[0:5].animate.set_color(WHITE),
-            lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-
-        self.wait(1)
-
-        for i in range(len(third_hamilton_path) - 1):
-            u = third_hamilton_path[i]
-            v = third_hamilton_path[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            line_index = get_index(u,v)
-
-            self.play(
-                lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-            self.wait(1)
-        self.play(
-            dots[0:5].animate.set_color(GREEN) ,
-            *[lines[
-                  get_index(
-                      third_hamilton_path[m],
-                      third_hamilton_path[m + 1]
-
-                  )
-              ].animate.set_color(GREEN) for m in range(len(third_hamilton_path) - 1)
-              ]
-        )
-
-        self.play(
-            dots[0:5].animate.set_color(WHITE),
-            lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-
-        self.wait(1)
-
-
-        extended_line_list = [
-            [3,14],
-            [3,5],
-            [15,10],
-            [15,9],
-            [11,5],
-            [8,13],
-            [13,11],
-            [6,7],
-            [8,14],
-            [9,3],
-            [9,7],
-            [10,12],
-            [12,6],
-            [6,13],
-            [3,11],
-            [12,14],
-            [10,5]
+        # --- Phase 2: Full graph (16 nodes, 23 edges) ---
+        extended_positions = [
+            [-1.5190050841020142, -0.555995671348672],   # 5
+            [1.87625422654442, 1.3674150662044973],      # 6
+            [3.355581091063108, 2.8278229998944537],     # 7
+            [2.144193665966549, -2.8786121097563795],    # 8
+            [-1.6778929303556458, 0.9683144634387277],   # 9
+            [-2.467284852119872, -0.2263938676336097],   # 10
+            [-4.09644323847631, -1.0267798936875705],    # 11
+            [0.13104734778506, 1.2031149280588904],      # 12
+            [2.477545942167559, 0.582829322973079],      # 13
+            [0.7878419899872489, 0.7838878005782175],    # 14
+            [-4.611300071071119, 0.6147144346102609],    # 15
         ]
-        tot_line = line_list + extended_line_list
-        def get_index(u,v):
-            try:
-                return tot_line.index([u, v])
-            except ValueError:
-                return tot_line.index([v, u])
+        extended_edges = [
+            [3, 14], [3, 5], [15, 10], [15, 9], [11, 5],
+            [8, 13], [13, 11], [6, 7], [8, 14], [9, 3],
+            [9, 7], [10, 12], [12, 6], [6, 13], [3, 11],
+            [12, 14], [10, 5],
+        ]
 
-        extended_lines = VGroup(
-            *[
-                Line(dots[i[0]], dots[i[1]]) for i in extended_line_list
-            ]
+        full_graph = PathFindingGraph(
+            dot_positions=small_positions + extended_positions,
+            edges=line_list + extended_edges,
+            show_labels=False,
         )
-        tot_lines = VGroup(*lines, *extended_lines)
+
+        # Hide new dots/lines before adding full graph to scene
+        for dot in full_graph.dots[5:]:
+            dot.set_opacity(0)
+        for line in full_graph.lines[6:]:
+            line.set_opacity(0)
+
+        self.remove(small_graph)
+        self.add(full_graph)
+
+        # Animate new nodes and edges appearing
         self.play(
-            FadeIn(extended_lines),
-            FadeIn(dots[5:]),
-            run_time=2
+            *[dot.animate.set_opacity(1) for dot in full_graph.dots[5:]],
+            *[line.animate.set_opacity(1) for line in full_graph.lines[6:]],
+            run_time=2,
         )
+
         self.wait(10)
-        first_long_non_hamilton = [3,14,8,13,6,7,9,15,10,5,11]
-        second_long_non_hamilton = [0,1,2,3,11,5]
-        for i in range(len(first_long_non_hamilton) - 1):
-            u = first_long_non_hamilton[i]
-            v = first_long_non_hamilton[i + 1]
 
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            line_index = get_index(u,v)
+        full_graph.show_non_hamilton_graph(self)
 
-            self.play(
-                tot_lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-        self.play(
-            *[dots[m].animate.set_color(RED) for m in first_long_non_hamilton],
-            *[tot_lines[
-                  get_index(
-                      first_long_non_hamilton[m],
-                      first_long_non_hamilton[m + 1]
-
-                  )
-              ].animate.set_color(RED) for m in range(len(first_long_non_hamilton) - 1)
-              ]
-        )
-
-        self.play(
-            dots.animate.set_color(WHITE),
-            tot_lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-
-
-
-        for i in range(len(second_long_non_hamilton) - 1):
-            u = second_long_non_hamilton[i]
-            v = second_long_non_hamilton[i + 1]
-
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            line_index = get_index(u,v)
-
-            self.play(
-                tot_lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-        self.play(
-            *[dots[m].animate.set_color(RED) for m in second_long_non_hamilton],
-            *[tot_lines[
-                  get_index(
-                      second_long_non_hamilton[m],
-                      second_long_non_hamilton[m + 1]
-
-                  )
-              ].animate.set_color(RED) for m in range(len(second_long_non_hamilton) - 1)
-              ]
-        )
-
-        self.play(
-            dots.animate.set_color(WHITE),
-            tot_lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-        n_p = Text("NP").shift(RIGHT*6)
+        n_p = Text("NP").shift(RIGHT * 6)
         self.play(Write(n_p))
 
-        hamilton_path = [4, 2, 1, 0, 3, 11, 13, 8, 14, 12, 6, 7, 9, 15, 10, 5]
-        for i in range(len(hamilton_path) - 1):
-            u = hamilton_path[i]
-            v = hamilton_path[i + 1]
+        full_graph.show_hamilton_graph(self)
 
-            # 4. ROBUST LOGIC: Check both [u,v] and [v,u]
-            # This prevents crashing if the path goes "backwards"
-            line_index = get_index(u,v)
-
-            self.play(
-                tot_lines[line_index].animate.set_color(YELLOW),
-                dots[u].animate.set_color(YELLOW),
-                # If it's the last step, color the final dot too
-                dots[v].animate.set_color(YELLOW) ,
-                run_time=0.5
-            )
-        self.play(
-            *[dots[m].animate.set_color(GREEN) for m in hamilton_path],
-            *[tot_lines[
-                  get_index(
-                      hamilton_path[m],
-                      hamilton_path[m + 1]
-
-                  )
-              ].animate.set_color(GREEN) for m in range(len(hamilton_path) - 1)
-              ]
-        )
-
-        self.play(
-            dots.animate.set_color(WHITE),
-            tot_lines.animate.set_color(WHITE),
-            run_time=0.5
-        )
-
-        p = Text("P").shift(LEFT*6)
+        p = Text("P").shift(LEFT * 6)
         self.play(Write(p))
-        P_equal_NP =VGroup(
+
+        P_equal_NP = VGroup(
             MathTex("P"),
             MathTex("="),
-            MathTex(" NP")
+            MathTex(" NP"),
         ).arrange(RIGHT).scale(2)
         self.play(
-            ReplacementTransform(p,P_equal_NP[0]),
-            ReplacementTransform(n_p,P_equal_NP[2]),
-            ReplacementTransform(VGroup(tot_lines,dots),P_equal_NP[1])
+            ReplacementTransform(p, P_equal_NP[0]),
+            ReplacementTransform(n_p, P_equal_NP[2]),
+            ReplacementTransform(full_graph, P_equal_NP[1]),
         )
-        big_question_mark = MathTex("?",color=RED).scale(4)
+        big_question_mark = MathTex("?", color=RED).scale(4)
         self.play(Write(big_question_mark))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- mobjects/path_finding_graph.py
+++ mobjects/path_finding_graph.py
@@ -1,29 +1,222 @@
 from manim import *
-import networkx as nx
-class PathFindingGraph(Graph):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+
+
+class PathFindingGraph(VGroup):
+    """A path-finding mobject that manages dots at given coordinates and lines between them."""
+
+    def __init__(self, dot_positions=None, edges=None, show_labels=True, label_type="index", **kwargs):
+        super().__init__(**kwargs)
+        self.dot_positions = dot_positions or []
+        self.edge_list = edges or []
         self.non_hamilton_path = []
         self.hamilton_path = []
+        self.show_labels = show_labels
+        self.label_type = label_type  # "index" or "coord"
 
+        # Create dots from coordinates
+        self.dots = VGroup()
+        self.lines = VGroup()
+        self.dot_labels = VGroup()
 
-    #hamilton graph
-    def _get_next_dot(self):
-        pass
+        for i, pos in enumerate(self.dot_positions):
+            dot = Dot(point=RIGHT * pos[0] + UP * pos[1])
+            self.dots.add(dot)
+            if self.show_labels:
+                label = self._make_label(i, pos)
+                label.next_to(dot, UR, buff=0.1)
+                self.dot_labels.add(label)
+
+        # Create lines between dots
+        for u, v in self.edge_list:
+            self.lines.add(Line(self.dots[u], self.dots[v]))
+
+        self.add(self.dots, self.lines)
+        if self.show_labels:
+            self.add(self.dot_labels)
+
+    def _make_label(self, index, pos):
+        """Create a label for a dot based on label_type."""
+        if self.label_type == "coord":
+            text = f"({pos[0]:.1f}, {pos[1]:.1f})"
+        else:
+            text = str(index)
+        return Text(text, font_size=18, color=YELLOW)
+
+    # --- Mutators ---
+
+    def add_dot(self, x, y):
+        """Add a new dot at coordinate (x, y). Returns the index of the new dot."""
+        dot = Dot(point=RIGHT * x + UP * y)
+        self.dot_positions.append([x, y])
+        self.dots.add(dot)
+        if self.show_labels:
+            label = self._make_label(len(self.dots) - 1, [x, y])
+            label.next_to(dot, UR, buff=0.1)
+            self.dot_labels.add(label)
+        return len(self.dots) - 1
+
+    def add_line(self, u, v):
+        """Add a line between dot indices u and v. Returns the index of the new line."""
+        line = Line(self.dots[u], self.dots[v])
+        self.edge_list.append([u, v])
+        self.lines.add(line)
+        return len(self.lines) - 1
+
+    def add_edges(self, edges):
+        """Add multiple edges at once. Each edge is [u, v]."""
+        for u, v in edges:
+            self.add_line(u, v)
+
+    # --- Queries ---
+
+    def get_edge_index(self, u, v):
+        """Return the index of the edge [u,v] or [v,u] in edge_list."""
+        try:
+            return self.edge_list.index([u, v])
+        except ValueError:
+            return self.edge_list.index([v, u])
+
+    def get_dot(self, index):
+        """Return the Dot mobject at the given index."""
+        return self.dots[index]
+
+    def get_line(self, index):
+        """Return the Line mobject at the given index."""
+        return self.lines[index]
+
+    def get_line_between(self, u, v):
+        """Return the Line mobject connecting dots u and v."""
+        return self.lines[self.get_edge_index(u, v)]
+
+    # --- Hamilton path ---
+
+    def _get_next_dot(self, dot, previous=None):
+        """Return a list of adjacent dot indices, excluding the previous dot."""
+        neighbors = []
+        for u, v in self.edge_list:
+            if u == dot and v != previous:
+                neighbors.append(v)
+            elif v == dot and u != previous:
+                neighbors.append(u)
+        return neighbors
 
     def find_non_hamilton_path(self):
-        pass
+        """Find a path that cannot be a Hamilton path using greedy DFS."""
+        if self.non_hamilton_path:
+            return self.non_hamilton_path
+
+        n = len(self.dot_positions)
+        for start in range(n):
+            visited = {start}
+            path = [start]
+            current = start
+            previous = None
+            while len(path) < n:
+                neighbors = self._get_next_dot(current, previous)
+                unvisited = [nb for nb in neighbors if nb not in visited]
+                if not unvisited:
+                    break
+                next_dot = unvisited[0]
+                visited.add(next_dot)
+                path.append(next_dot)
+                previous = current
+                current = next_dot
+            if len(path) < n:
+                self.non_hamilton_path = path
+                return self.non_hamilton_path
+
+        self.non_hamilton_path = []
+        return self.non_hamilton_path
 
     def find_hamilton_path(self):
+        """Find a Hamilton path using backtracking DFS."""
         if self.hamilton_path:
-            pass
-        else:
-            self.hamilton_path = nx.hamiltonian_path(self.graph)
+            return self.hamilton_path
 
+        n = len(self.dot_positions)
 
+        def backtrack(path, visited):
+            if len(path) == n:
+                return path
+            current = path[-1]
+            for nb in self._get_next_dot(current):
+                if nb not in visited:
+                    visited.add(nb)
+                    path.append(nb)
+                    result = backtrack(path, visited)
+                    if result:
+                        return result
+                    path.pop()
+                    visited.discard(nb)
+            return None
 
-    def show_non_hamilton_graph(self):
-        pass
+        for start in range(n):
+            result = backtrack([start], {start})
+            if result:
+                self.hamilton_path = list(result)
+                return self.hamilton_path
 
-    def show_hamilton_graph(self):
-        pass
+        self.hamilton_path = []
+        return self.hamilton_path
+
+    def show_non_hamilton_graph(self, scene, run_time=0.5):
+        """Animate a non-Hamilton path: yellow while exploring, red on failure, then reset."""
+        if not self.non_hamilton_path:
+            self.find_non_hamilton_path()
+        if not self.non_hamilton_path:
+            return
+
+        path = self.non_hamilton_path
+
+        for i in range(len(path) - 1):
+            u, v = path[i], path[i + 1]
+            line = self.get_line_between(u, v)
+            scene.play(
+                line.animate.set_color(YELLOW),
+                self.dots[u].animate.set_color(YELLOW),
+                self.dots[v].animate.set_color(YELLOW),
+                run_time=run_time,
+            )
+
+        scene.play(
+            *[self.dots[m].animate.set_color(RED) for m in path],
+            *[self.get_line_between(path[m], path[m + 1]).animate.set_color(RED)
+              for m in range(len(path) - 1)],
+        )
+
+        scene.play(
+            self.dots.animate.set_color(WHITE),
+            self.lines.animate.set_color(WHITE),
+            run_time=run_time,
+        )
+
+    def show_hamilton_graph(self, scene, run_time=0.5):
+        """Animate the Hamilton path: yellow while exploring, green on success, then reset."""
+        if not self.hamilton_path:
+            self.find_hamilton_path()
+        if not self.hamilton_path:
+            return
+
+        path = list(self.hamilton_path)
+
+        for i in range(len(path) - 1):
+            u, v = path[i], path[i + 1]
+            line = self.get_line_between(u, v)
+            scene.play(
+                line.animate.set_color(YELLOW),
+                self.dots[u].animate.set_color(YELLOW),
+                self.dots[v].animate.set_color(YELLOW),
+                run_time=run_time,
+            )
+
+        scene.play(
+            *[self.dots[m].animate.set_color(GREEN) for m in path],
+            *[self.get_line_between(path[m], path[m + 1]).animate.set_color(GREEN)
+              for m in range(len(path) - 1)],
+        )
+
+        scene.play(
+            self.dots.animate.set_color(WHITE),
+            self.lines.animate.set_color(WHITE),
+            run_time=run_time,
+        )
